4823bb8fbecf0c40ea4de2925256ea1b9e7cfef5
MerayoS <santi.merayo@gmail.com>
2022-06-07 11:20:24 +0200

    modificacion comentario 1

--- ejercicio 1/Program.cs
+++ ejercicio 1/Program.cs
@@ -10,6 +10,7 @@ namespace ejercicio_1
     {
         static void Main(string[] args)
         {
+            //modificacion 123
             while (true)
             {
                 Console.WriteLine("Seleccione el proyecto: ");
